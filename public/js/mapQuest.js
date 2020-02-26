092c7fb1b0c56dd68a14351fb5b52c01e441ef11
Jeffrey Lau <j8lau@ucsd.edu>
2020-02-26 12:45:57 -0800

    finished

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,15 @@
 function initMap() {
 	// add your code here
+	L.mapquest.key = "ccbi8Vr9mGRy5GOmtdCtNLCBhX3HdYVM";
+
+	// 'map' refers to a <div> element with the ID map
+	var map = L.mapquest.map('map', {
+		center: [32.878810, -117.235910],
+		layers: L.mapquest.tileLayer('map'),
+		zoom: 12, 
+		zoomControl: false
+	});
+
+	// Add a marker to the map
+	L.marker([32.878810, -117.235910]).addTo(map);
 }
